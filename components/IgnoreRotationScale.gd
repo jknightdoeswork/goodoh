6bfcc52509d8dd1e49eca9daa4f060eaf5bdd5fd
Jason Knight <00jknight@gmail.com>
2019-12-04 22:27:20 -0600

    IgnoreRotationScale added Ready.

--- components/IgnoreRotationScale.gd
+++ components/IgnoreRotationScale.gd
@@ -1,19 +1,27 @@
 extends Node
 class_name IgnoreRotationScale
 
-enum UpdateFunction {Process, PhysicsProcess}
+enum UpdateFunction {Process, PhysicsProcess, Ready}
 export(UpdateFunction) var update_function = UpdateFunction.Process
 
 func _ready():
 	var parent = get_parent()
 	assert(parent is Spatial)
 	assert(parent.get_parent() is Spatial)
+	var prev_transform = parent.transform
 	parent.set_as_toplevel(true)
+	parent.transform = prev_transform
+	
+	if update_function == UpdateFunction.Ready:
+		do()
+		
+func do():
+	get_parent().translation = get_parent().get_parent().translation
 	
 func _process(_delta):
 	if update_function == UpdateFunction.Process:
-		get_parent().translation = get_parent().get_parent().translation
+		do()
 
 func _physics_process(_delta):
 	if update_function == UpdateFunction.PhysicsProcess:
-		get_parent().translation = get_parent().get_parent().translation
+		do()
