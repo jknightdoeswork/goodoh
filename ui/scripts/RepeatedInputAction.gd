82da49eb106c352804a4b7073a0d5e7e2da684e5
Jason Knight <00jknight@gmail.com>
2020-01-19 23:00:39 -0600

    repeat interval

--- ui/scripts/RepeatedInputAction.gd
+++ ui/scripts/RepeatedInputAction.gd
@@ -7,7 +7,7 @@ signal action_triggered
 export(String) var action_name
 
 #var trigger_times := [0.2, 0.1, 0.1, 0.05, 0.05, 0.1, 0.05, 0.025, 0.025, 0.025, 0.025, 0.025, 0.025, 0.025, 0.025, 0.01]
-var trigger_times := [0.1, 0.05, 0.05, 0.01]
+var trigger_times := [0.2, 0.01, 0.05, 0.01]
 
 var next_press_time := 0.0
 var trigger_count := 0
