e1d10c41c84201975ec2b55c7f56c2288aa06455
Jason Knight <00jknight@gmail.com>
2020-02-19 01:03:51 -0600

    start animation hacky script

--- components/StartAnimation.gd
+++ components/StartAnimation.gd
@@ -2,9 +2,13 @@ extends Node
 
 export var animation_name := ""
 export var looping := false
+export var node_path := ""
 
 func _ready():
-	var p = get_parent()
+	var p = get_node(node_path)
 	assert(p is AnimationPlayer)
-	
+	var animation = p.get_animation(animation_name) as Animation
+	if animation != null:
+		print (animation.loop)
+		animation.loop = looping
 	p.play(animation_name)
