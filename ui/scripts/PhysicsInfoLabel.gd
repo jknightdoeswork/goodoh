2e0b1aa93e8ab1def0976691e5dbda91e0235671
Jason Knight <00jknight@gmail.com>
2020-03-15 09:21:24 -0600

    Underscore in process delta.

--- ui/scripts/PhysicsInfoLabel.gd
+++ ui/scripts/PhysicsInfoLabel.gd
@@ -1,6 +1,6 @@
 extends Label
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	self.text = "active_objects: %s\ncollision_pairs: %s\nisland_count: %s" % [
 		Physics2DServer.get_process_info(Physics2DServer.INFO_ACTIVE_OBJECTS),
 		Physics2DServer.get_process_info(Physics2DServer.INFO_COLLISION_PAIRS),
