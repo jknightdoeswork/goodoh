e31ef70f8818cfcd330c78e2ea60c5fff365b822
Jason Knight <00jknight@gmail.com>
2020-03-21 20:02:27 -0600

    perf

--- perf/Perf.gd
+++ perf/Perf.gd
@@ -5,6 +5,8 @@ var sample_stack := []
 func begin_sample():
 	sample_stack.push_front(OS.get_ticks_usec())
 func end_sample(s:String):
-	var delta = OS.get_ticks_usec() - sample_stack.pop_front()
+	var now := OS.get_ticks_usec()
+	var then:int = sample_stack.pop_front()
+	var delta = (now - then) / 1000.0
 	print ("[Perf]\t%s\t\t%s"%[s, delta])
 	
