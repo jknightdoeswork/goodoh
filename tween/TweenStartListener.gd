21cef2ace023e9be6208392c1836c257d4b6f5ee
Jason Knight <00jknight@gmail.com>
2020-04-21 08:33:36 -0600

    intial value explicit sets on tween start

--- tween/TweenStartListener.gd
+++ tween/TweenStartListener.gd
@@ -7,11 +7,12 @@ onready var tween = get_parent().get_node("Tween")
 export(Resource) var tween_data
 export var cancel_current_on_start := true
 export var make_tween_data := false setget set_make_tween_data
-
+export var start_on_ready := false
 func do():
 	var t:TweenData = tween_data
 	var i = null
 	if t.initial_value_type == TweenData.InitialValueType.Explicit:
+		target_node.set(tween_data.property, tween_data.initial_value())
 		i = t.initial_value()
 	elif t.initial_value_type == TweenData.InitialValueType.PreviousFinal:
 		pass
@@ -23,3 +24,8 @@ func set_make_tween_data(b:bool):
 	if b:
 		tween_data = TweenData.new()
 		property_list_changed_notify()
+
+func _ready():
+	if start_on_ready:
+		do()
+		
