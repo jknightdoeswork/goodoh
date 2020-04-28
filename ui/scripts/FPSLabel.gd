6aa4f4cd871f87c466275fb4987cc4e4237ecde6
Jason Knight <00jknight@gmail.com>
2020-04-28 14:20:37 -0600

    only update fps if visible

--- ui/scripts/FPSLabel.gd
+++ ui/scripts/FPSLabel.gd
@@ -2,4 +2,5 @@ extends Label
 class_name FPSLabel
 
 func _process(_delta):
-	self.text = str(Engine.get_frames_per_second()) + "FPS"
+	if self.is_visible_in_tree():
+		self.text = str(Engine.get_frames_per_second()) + "FPS"
