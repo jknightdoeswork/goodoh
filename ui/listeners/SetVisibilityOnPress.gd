8edc6f37a98f4ea3a875707bec4e05e26bad607b
Jason Knight <00jknight@gmail.com>
2020-05-26 21:38:18 -0600

    Button component to change visibilitiy

---
+++ ui/listeners/SetVisibilityOnPress.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+export(NodePath) var target
+export var set_visible_to := true
+
+onready var target_ = get_node(target) as CanvasItem
+onready var parent := get_parent() as Button
+
+func _ready():
+	assert(parent != null)
+	assert(target_ != null)
+	parent.connect("pressed", self, "on_button_pressed")
+
+func on_button_pressed():
+	target_.visible = set_visible_to
