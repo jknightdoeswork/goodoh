c74a2f001f90783354951e25fb3844460e121f64
Jason Knight <00jknight@gmail.com>
2020-09-05 13:34:07 -0600

    Play Animation Listener assert pattern changed for typing purposes.

--- listeners/PlayAnimationListener.gd
+++ listeners/PlayAnimationListener.gd
@@ -3,6 +3,6 @@ extends Node
 export var animation_name := ""
 
 func do():
-	var p = get_parent()
-	assert(p is AnimationPlayer)
+	var p :AnimationPlayer= get_parent()
+	assert(p != null)
 	p.play(animation_name)
