b46d424e61ee28fb84ab1a202f2c10b67876485a
Jason Knight <00jknight@gmail.com>
2020-09-16 08:02:16 -0600

    Lowercase fps

--- ui/scripts/FPSLabel.gd
+++ ui/scripts/FPSLabel.gd
@@ -3,4 +3,4 @@ class_name FPSLabel
 
 func _process(_delta):
 	if self.is_visible_in_tree():
-		self.text = str(Engine.get_frames_per_second()) + "FPS"
+		self.text = str(Engine.get_frames_per_second()) + "fps"
