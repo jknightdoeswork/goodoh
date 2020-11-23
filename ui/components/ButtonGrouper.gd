94907e23838b956730d8dcf720270d387078f0bf
Jason Knight <00jknight@gmail.com>
2020-11-22 20:29:55 -0600

    button grouper

---
+++ ui/components/ButtonGrouper.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name ButtonGrouper
+
+signal changed
+
+onready var parent_control := get_parent() as Control
+onready var button_group := ButtonGroup.new()
+onready var buttons := []
+var selected:int
+
+func _ready():
+	for i in parent_control.get_children():
+		if i is Button:
+			var b := i as Button
+			b.group = button_group
+			b.connect("pressed", self, "button_pressed")
+			buttons.append(b)
+	button_pressed()
+
+func button_pressed():
+	selected = buttons.find(button_group.get_pressed_button())
+	print("selected_button: " + str(selected))
+	self.emit_signal("changed")
